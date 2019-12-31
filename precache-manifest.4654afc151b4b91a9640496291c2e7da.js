7e7fde3decb0aa7e3ca4a268f28252cacb8e9b80
Michael Milton <ttmigueltt@gmail.com>
2019-12-31 18:33:08 +1100

    Updates

---
+++ precache-manifest.4654afc151b4b91a9640496291c2e7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7070cbdc3106756af2907e45aa25f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "435a12bcdc8e189d1fab",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f72134161b1944b9a3c5",
+    "url": "/static/js/2.16bd7a2a.chunk.js"
+  },
+  {
+    "revision": "26940e70f62ce4f8bbd320ef0fde83ed",
+    "url": "/static/js/2.16bd7a2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "435a12bcdc8e189d1fab",
+    "url": "/static/js/main.2310bb17.chunk.js"
+  },
+  {
+    "revision": "6c2595e801bb44c5d230",
+    "url": "/static/js/runtime-main.629f34fc.js"
+  }
+]);
